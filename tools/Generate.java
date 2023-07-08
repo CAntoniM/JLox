5a783792aaf1e8cb0853354db10576e5684089e2
Callum <Callum@cantonim.me>
2023-07-08 18:17:39 +0100

    updating the generator to produce the a visitor pattern

--- tools/Generate.java
+++ tools/Generate.java
@@ -19,9 +19,24 @@ public class Generate {
         writer.println("        }");
 
         writer.println();
+        writer.println("        @Override");
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" + className + baseName + "(this);");
+        writer.println("        }");
 
         for (String feild: feilds) {
             writer.println("        public final " + feild + ";");
+        }
+
+        writer.println("    }");
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("        R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
         }
 
         writer.println("    }");
@@ -38,11 +53,16 @@ public class Generate {
         writer.println();
         writer.println("public abstract class " + baseName + " {");
 
+        defineVisitor(writer,baseName, types);
+
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String feilds = type.split(":")[1].trim();
             defineType(writer, baseName, className, feilds);
         }
+
+        writer.println();
+        writer.println("    abstract <R> R accept( Visitor<R> visitor);");
 
         writer.println("}");
         writer.close();
